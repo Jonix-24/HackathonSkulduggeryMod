4ef0c705be500e521c40f9fe9fba9350af9433db
unknown <kristianphilipp13@gmail.com>
2025-09-26 19:44:28 +0200

    added zombie that gets more damage if its hot 2

---
+++ Content/Enemies/Hollow.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.Bestary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader.Utilities
+
+using Terraria.WorldBuilding;
+
+namespace HackathonSkulduggeryMod.Content.Enemies
+{
+    internal class Hollow : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            NPC.CloneDefaults(NPCID.Zombie);
+            if (NPC.onFire)
+            {
+                NPC.defense = 1;
+            }
+        }
+    }
+}
